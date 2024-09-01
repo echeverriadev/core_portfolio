9ce4cefaf47016845daf6b6fc2a345226579fa69
Carlos Echeverría <55212109+echeverriadev@users.noreply.github.com>
2024-09-01 17:52:12 -0500

    feat: add super user seed (#4)

--- src/seeds/seeds.ts
+++ src/seeds/seeds.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../app.module';
-import { PermissionsService } from '../permissions/services/permissions.service';
-import { RolesService } from '../roles/services/roles.service';
 import { Permission } from '../permissions/schemas/permission.schema';
+import { PermissionsService } from '../permissions/services/permissions.service';
 import { Role } from '../roles/schemas/role.schema';
+import { RolesService } from '../roles/services/roles.service';
+import { User } from '../users/schemas/user.schema';
+import { UsersService } from '../users/services/users.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   const permissionsService = app.get(PermissionsService);
   const rolesService = app.get(RolesService);
+  const usersService = app.get(UsersService);
 
   const permissions = [
     { name: 'all_permissions', description: 'All permissions' },
@@ -29,6 +32,17 @@ async function bootstrap() {
   for (const role of roles) {
     await rolesService.create(role);
   }
+
+  const user = {
+    email: 'echeverriadev@gmail.com',
+    password: '123456',
+    roles: (await rolesService.findAll()).map(r => r._id.toString()) as string[],
+    name: 'Carlos',
+    lastName: 'Echeverria',
+    phone: '3105891873',
+  } as unknown as User;
+
+  await usersService.create(user);
 
   await app.close();
 }
